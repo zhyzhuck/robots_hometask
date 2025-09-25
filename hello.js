342af4318c1e48832cd9a03ee671f8cb7dea1cf6
t.yatsukhnenko <t.yatsukhnenko@gmail.com>
2025-09-25 23:17:23 +0300

    add text

--- hello.js
+++ hello.js
@@ -1,7 +1,29 @@
 let a = 0;
-b = 4;
+const b = 4;
 console.log(a + b);
 
 a = 467;
+
+console.log(a + b*2);
+console.log(a + b);
+
+a = 4675;
+
+console.log(a + b*2);
+
+console.log(a + b);
+
+a = 4674;
+
+console.log(a + b*2);
+console.log(a + b);
+
+a = 4673;
+
+console.log(a + b*2);
+
+console.log(a + b);
+
+a = 4672;
 
 console.log(a + b*2);
